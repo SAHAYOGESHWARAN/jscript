91c7c776d79824fa8ec45ba0118ac8e55c784362
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-26 08:33:52 +0530

    add jscript master

--- extending function.js
+++ extending function.js
@@ -1,21 +1,21 @@
-const Car = function(color){
+const Car = function(color) {
     this.color = color;
-  
 };
+
 Car.prototype.getColor = function() {
     return this.color;
-}
-const ToyCar = function(color,model){
-    Car.call(this,color);
+};
+
+const ToyCar = function(color, model) {
+    Car.call(this, color);  
     this.model = model;
 };
+
 ToyCar.prototype = Object.create(Car.prototype);
-ToyCar.prototype.getModel = function(){
+ToyCar.prototype.constructor = ToyCar; 
+ToyCar.prototype.getModel = function() {
     return this.model;
-}
+};
+
 const speedyBlueCar = new ToyCar('blue', 'speedy');
-console.log(speedyBlueCar.getColor(),speedyBlueCar.getModel());
-
-
-//output
-//blue speedy
+console.log(speedyBlueCar.getColor(), speedyBlueCar.getModel());  // Outputs: 'blue speedy'
