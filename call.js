51d8a719a76b811b878a27ff5e0a9d2581fba24d
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-26 09:40:50 +0530

    add jscript master

--- call.js
+++ call.js
@@ -1,16 +1,17 @@
-const profile ={
-    name:"saha",
-    age:20
-
+const profile = {
+    name: "saha",
+    age: 20
 };
 
-const setProfile = function(name,age){
+const setProfile = function(name, age) {
     this.name = name;
     this.age = age;
 };
 
-setProfile.call(profile,'gokul',30);
-console.log(profile);
+let params = ['gokul', 30];
+setProfile.apply(profile, params); 
 
-//output
-//{ name: 'gokul', age: 30 }
+console.log(profile); 
+
+// Output
+ //{ name: 'gokul', age: 30 }
