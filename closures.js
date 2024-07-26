b56d67cc132f30d136e9319beae640a20f07289e
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-26 10:11:36 +0530

    add jscript master

--- closures.js
+++ closures.js
@@ -1,19 +1,23 @@
-const x =() => {
-    let i = 0;
-    let J = 1;
-
-    const returnX =() => {
-        j++;
-        console.log(i);
-        };
-        return{
-            returnX
-        }
+const counter =() => {
+    let i =0;
+    const add = (val) => {
+        i += val;
     };
+    const subtract = (val) => {
+        i -= val;
+    }
+const print =() =>{
+    return i;
+}
+return {add,subtract,print};
+};
+const outerCounter = counter();
 
- x().returnX();
+//console.log(outerCounter);
+outerCounter.add(2);
+
+console.log(outerCounter.print());
 
 
-
-    //output 
-    //undefined
+/// output
+// 2
