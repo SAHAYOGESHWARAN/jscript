fe0a99543dba1d11ac5b5f825f7dfe4dc563f5b7
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-26 10:26:55 +0530

    add jscript master

---
+++ function chain.js
@@ -0,0 +1,26 @@
+const counter = () => {
+    let i = 0;
+
+    return {
+        add(val) {
+            i += val;
+            return this; 
+        },
+        subtract(val) {
+            i -= val;
+            return this; 
+        },
+        print() {
+            console.log(i);
+            return this; 
+        }
+    };
+};
+
+const outerCounter = counter();
+
+outerCounter.add(2).subtract(1).print(); 
+
+
+
+// Output: 1
