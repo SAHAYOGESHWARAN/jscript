9df56f97737bb3f85074ca16a4d3d2dbabefdf30
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-26 09:14:58 +0530

    add jscript master

--- arrowfunction.js
+++ arrowfunction.js
@@ -1,7 +1,26 @@
-const add = function(a,b){
-    return a+b;
+const add = function(a, b) {
+    return a + b;
+};
 
+const addArrow = (a, b) => a + b;
+const square = a => a * a;
+const squareObj = a => ({ square: a * a });
+
+const getArguments = (...x) => {
+    console.log(x);
 };
-const addArrow =(a,b) => {
-    return a+b;
+
+getArguments(1, 2);
+
+const Car = function(color) {
+    let setColor = () => {
+        this._color = color;
+    };
+    setColor();
 };
+
+const blueCar = new Car('blue');
+console.log(blueCar); 
+
+
+// Output: Car { _color: 'blue' }
